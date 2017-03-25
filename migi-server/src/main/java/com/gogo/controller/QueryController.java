14281b84b8798af086aa8f58cad4a46312df2856
ImaduddinAMajid <imad@accelbyte.net>
2017-03-25 15:11:06 +0700

    add Message model

--- migi-server/src/main/java/com/gogo/controller/QueryController.java
+++ migi-server/src/main/java/com/gogo/controller/QueryController.java
@@ -1,5 +1,6 @@
 package com.gogo.controller;
 
+import com.gogo.model.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QueryController {
 
-    @RequestMapping(value = "/query" ,method = RequestMethod.GET, params = "username")
-    public ResponseEntity<?> getUserByUsername(@RequestParam("username") String username) {
-        return new ResponseEntity("Hi " + username, HttpStatus.OK);
+    @RequestMapping(value = "/query" ,method = RequestMethod.GET, params = "message")
+    public ResponseEntity<?> getUserByUsername(@RequestParam("message") String message) {
+        Message msg = new Message(message);
+        return new ResponseEntity(msg, HttpStatus.OK);
     }
 
 }
