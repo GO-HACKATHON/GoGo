743e62e7cb8304c9df080de77a00c2a7a939537f
Adi Prasetyo <adiprasetyo27@gmail.com>
2017-03-26 10:11:34 +0700

    fix bug

--- migi-android/app/src/main/java/com/gogo/migi/MainActivity.java
+++ migi-android/app/src/main/java/com/gogo/migi/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gogo.migi.config.Config;
 import com.gogo.migi.model.Chat;
 import com.loopj.android.http.AsyncHttpClient;
@@ -14,6 +16,7 @@ import com.loopj.android.http.RequestParams;
 import cz.msebera.android.httpclient.Header;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private ChatAdapter chatAdapter;
     private ArrayList<Chat> list;
+    private Boolean firstUse = true;
+    private String addressFrom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +38,18 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.etMessage);
 
         list = new ArrayList<Chat>();
+        updateAdapter("Hi, I'm Migi. \nI'm your transjogja assistant. \n Would you tell me where are you now? So I can assist you :)");
     }
 
     public void sendClicked(View view) throws UnsupportedEncodingException {
+        if (firstUse) {
+            this.addressFrom = editText.getText().toString();
+        }
+
         String message = this.editText.getText().toString();
         this.editText.setText("");
 
-        sendQuery(Config.URL + "/query?message=" + message);
+        sendQuery(Config.URL + "/query?message=" + message + "&addressFrom=" + this.addressFrom);
 
         updateAdapter(message);
     }
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendQuery(String url){
         RequestParams params = new RequestParams();
+        final ObjectMapper objectMapper = new ObjectMapper();
 
         //params.put("message", message);
 
@@ -65,7 +76,13 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        updateAdapter(response.toString());
+                        try {
+                            JsonNode rootNode = objectMapper.readTree(String.valueOf(response));
+
+                            updateAdapter(rootNode.get("message").asText());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         editText.setText("");
                     }
                 });
